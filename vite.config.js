caae121366e00b76e31b1b38c900d4b5a1d875b4
Mohamed Adel Saleh <mohamed.adel.saleh77@gmail.com>
2023-05-13 20:23:15 +0300

    solve issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ILLA-task/",
+  base: "/Pokemon-Gallary/",
   plugins: [react()],
 })
